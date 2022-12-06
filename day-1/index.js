5e6cf3c0cf5f64647aa04b8fbb0470185556befe
Sophie837 <sophieconstable@yahoo.co.uk>
2022-12-06 13:24:56 +0000

    day 1 challenge b

--- day-1/index.js
+++ day-1/index.js
@@ -1,8 +1,13 @@
 const { readFileSync, promises: fsPromises } = require("fs");
-const inventory = readFileSync("day-1/a/input.txt", "utf-8").split("\n\n");
+const inventory = readFileSync("day-1/input.txt", "utf-8").split("\n\n");
 
 const totalCalloriesPerReindeer = inventory.map((reindeer) =>
   reindeer.split("\n").reduce((a, b) => Number(a) + Number(b), 0)
 );
-const greatestCalloryCount = Math.max(...totalCalloriesPerReindeer);
-console.log(greatestCalloryCount);
+const topCalloryCount = Math.max(...totalCalloriesPerReindeer);
+console.log(topCalloryCount);
+const topThreeCalloryCount = totalCalloriesPerReindeer
+  .sort((a, b) => b - a)
+  .slice(0, 3)
+  .reduce((a, b) => a + b, 0);
+console.log(topThreeCalloryCount);
