03211430168a139bc6eb0ce86684e0795d4d3356
Sophie837 <sophieconstable@yahoo.co.uk>
2022-12-08 17:03:54 +0000

    day 4 challenge 1

---
+++ day-4/index.js
@@ -0,0 +1,22 @@
+const { readFileSync, promises: fsPromises } = require("fs");
+const allAssignmentPairs = readFileSync("day-4/input.txt", "utf-8")
+  .split("\n")
+  .filter((pair) => pair.length > 0);
+
+function sumOfFullyContainedAssignmentPairs() {
+  let fullyContainedAssignmentPairs = 0;
+  allAssignmentPairs.map((assignmentPair) => {
+    const pairBoundary = assignmentPair.match(/\d+/g);
+    if (
+      (Number(pairBoundary[2]) >= Number(pairBoundary[0]) &&
+        Number(pairBoundary[3]) <= Number(pairBoundary[1])) ||
+      (Number(pairBoundary[0]) >= Number(pairBoundary[2]) &&
+        Number(pairBoundary[1]) <= Number(pairBoundary[3]))
+    ) {
+      fullyContainedAssignmentPairs += 1;
+    }
+  });
+  console.log(fullyContainedAssignmentPairs);
+}
+
+sumOfFullyContainedAssignmentPairs();
