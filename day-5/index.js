a484642ff89ba19691ba6a70ff6ba6f992c2b7ff
Sophie837 <sophieconstable@yahoo.co.uk>
2022-12-09 15:44:40 +0000

    day 5 challenge 2

--- day-5/index.js
+++ day-5/index.js
@@ -4,6 +4,9 @@ const supplyStackInfo = readFileSync("day-5/input.txt", "utf-8").split("\n");
 const separator = supplyStackInfo.indexOf("");
 const supplyStackDiagram = supplyStackInfo.slice(0, separator);
 const supplyStackInstructions = supplyStackInfo.slice(separator + 1);
+const instructionNumbers = supplyStackInstructions.map((instruction) =>
+  instruction.match(/\d+/g)
+);
 const cargoCrateDiagram = {};
 
 function createCargoCrateDiagram() {
@@ -29,10 +32,7 @@ function createCargoCrateDiagram() {
   });
 }
 
-function operateCraneInstructions() {
-  const instructionNumbers = supplyStackInstructions.map((instruction) =>
-    instruction.match(/\d+/g)
-  );
+function operateCrateMover9000Instructions() {
   instructionNumbers.map((instruction) => {
     const originStack = instruction[1];
     const destinationStack = instruction[2];
@@ -44,6 +44,19 @@ function operateCraneInstructions() {
         ...crateToMove,
       ];
     }
+  });
+}
+
+function operateCrateMover9001Instructions() {
+  instructionNumbers.map((instruction) => {
+    const originStack = instruction[1];
+    const destinationStack = instruction[2];
+    const quantity = instruction[0];
+    const cratesToMove = cargoCrateDiagram[originStack].splice(-quantity);
+    cargoCrateDiagram[destinationStack] = [
+      ...cargoCrateDiagram[destinationStack],
+      ...cratesToMove,
+    ];
   });
 }
 
@@ -59,6 +72,17 @@ function identifyTopCrates() {
   console.log(topCratesCombined);
 }
 
-createCargoCrateDiagram();
-operateCraneInstructions();
-identifyTopCrates();
+function operateCrateMover9000() {
+  createCargoCrateDiagram();
+  operateCrateMover9000Instructions();
+  identifyTopCrates();
+}
+
+function operateCrateMover9001() {
+  createCargoCrateDiagram();
+  operateCrateMover9001Instructions();
+  identifyTopCrates();
+}
+
+// operateCrateMover9000();
+operateCrateMover9001();
