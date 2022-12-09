46fa0427fb8310ce4cd73316a476e05b0a124344
Sophie837 <sophieconstable@yahoo.co.uk>
2022-12-09 20:47:55 +0000

    day 6 challenge 2

--- day-6/index.js
+++ day-6/index.js
@@ -1,10 +1,17 @@
 const { readFileSync, promises: fsPromises } = require("fs");
 const datastreamBuffer = readFileSync("day-6/input.txt", "utf-8").split("");
 
-for (let i = 0; i < datastreamBuffer.length; i++) {
-  const fourCharacterSection = datastreamBuffer.slice(i, i + 4);
-  const numberOfUniqueCharacters = new Set(fourCharacterSection).size;
-  if (numberOfUniqueCharacters === 4) {
-    return i + 4;
+function findMarker(n) {
+  for (let i = 0; i < datastreamBuffer.length; i++) {
+    const fourCharacterSection = datastreamBuffer.slice(i, i + n);
+    const numberOfUniqueCharacters = new Set(fourCharacterSection).size;
+    if (numberOfUniqueCharacters === n) {
+      return i + n;
+    }
   }
 }
+
+const startOfPacketMarker = findMarker(4);
+const startOfMessageMarker = findMarker(14);
+console.log(startOfPacketMarker);
+console.log(startOfMessageMarker);
