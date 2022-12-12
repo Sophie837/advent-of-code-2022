b3485ee689833b9e43d8a8831c51cfeaef4cf114
Sophie837 <sophieconstable@yahoo.co.uk>
2022-12-12 21:39:51 +0000

    refactor

--- day-8/index.js
+++ day-8/index.js
@@ -1,42 +1,61 @@
 const { readFileSync, promises: fsPromises } = require("fs");
 const forestMap = readFileSync("day-8/input.txt", "utf-8").split("\n");
-const width = forestMap[0].length;
-const height = forestMap.length;
-const allTrees = width * height;
+const horizontalTrees = {};
+const allTrees = forestMap[0].length * forestMap.length;
 let hiddenTrees = 0;
 
-forestMap.forEach((row, yIndex) => {
-  const xTrees = row.split("");
-  for (let xIndex = 0; xIndex < xTrees.length; xIndex++) {
-    const tallerTreesToLeft =
-      xTrees.slice(0, xIndex).filter((tree) => tree >= row[xIndex]).length > 0
-        ? true
-        : false;
-    const tallerTreesToRight =
-      xTrees.slice(xIndex + 1).filter((tree) => tree >= row[xIndex]).length > 0
-        ? true
-        : false;
-    if (tallerTreesToLeft && tallerTreesToRight) {
-      let yTrees = [];
-      for (let j = 0; j < forestMap.length; j++) {
-        yTrees = [...yTrees, forestMap[j][xIndex]];
-      }
-      const tallerTreesAbove =
-        yTrees.slice(0, yIndex).filter((tree) => tree >= row[xIndex]).length > 0
+function getNumberOfTallerTreesLeftOrAbove(axis, sliceIndex, height) {
+  return axis.slice(0, sliceIndex).filter((tree) => tree >= height).length;
+}
+
+function getNumberOfTallerTreesRightOrBelow(axis, sliceIndex, height) {
+  return axis.slice(sliceIndex + 1).filter((tree) => tree >= height).length;
+}
+
+function getHorizontalTrees(xIndex) {
+  if (horizontalTrees[xIndex]) {
+    return horizontalTrees[xIndex];
+  } else {
+    const yTrees = forestMap.map((row) => row[xIndex]);
+    horizontalTrees[xIndex] = yTrees;
+    return yTrees;
+  }
+}
+
+function getNumberOfHiddenTrees() {
+  forestMap.forEach((row, yIndex) => {
+    const xTrees = row.split("");
+    for (let xIndex = 0; xIndex < xTrees.length; xIndex++) {
+      const tallerTreesToLeft =
+        getNumberOfTallerTreesLeftOrAbove(xTrees, xIndex, row[xIndex]) > 0
           ? true
           : false;
-      const tallerTreesBelow =
-        yTrees.slice(yIndex + 1).filter((tree) => tree >= row[xIndex]).length >
-        0
+      const tallerTreesToRight =
+        getNumberOfTallerTreesRightOrBelow(xTrees, xIndex, row[xIndex]) > 0
           ? true
           : false;
-      if (tallerTreesAbove && tallerTreesBelow) {
-        hiddenTrees += 1;
+      if (tallerTreesToLeft && tallerTreesToRight) {
+        const yTrees = getHorizontalTrees(xIndex);
+        const tallerTreesAbove =
+          getNumberOfTallerTreesLeftOrAbove(yTrees, yIndex, row[xIndex]) > 0
+            ? true
+            : false;
+        const tallerTreesBelow =
+          getNumberOfTallerTreesRightOrBelow(yTrees, yIndex, row[xIndex]) > 0
+            ? true
+            : false;
+        if (tallerTreesAbove && tallerTreesBelow) {
+          hiddenTrees += 1;
+        }
       }
     }
-  }
-});
+  });
+  console.log("Hidden: ", hiddenTrees);
+}
 
-console.log("Hidden: ", hiddenTrees);
-const visibleTrees = allTrees - hiddenTrees;
-console.log("Visible: ", visibleTrees);
+function getNumberOfVisibleTrees() {
+  console.log("Visible: ", allTrees - hiddenTrees);
+}
+
+getNumberOfHiddenTrees();
+getNumberOfVisibleTrees();
