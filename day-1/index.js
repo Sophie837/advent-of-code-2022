c8a8379ee2af7ed28b4a5d045e68694e7341daba
Sophie837 <sophieconstable@yahoo.co.uk>
2022-12-13 10:57:00 +0000

    typo

--- day-1/index.js
+++ day-1/index.js
@@ -1,13 +1,13 @@
 const { readFileSync, promises: fsPromises } = require("fs");
 const inventory = readFileSync("day-1/input.txt", "utf-8").split("\n\n");
 
-const totalCalloriesPerReindeer = inventory.map((reindeer) =>
+const totalCaloriesPerReindeer = inventory.map((reindeer) =>
   reindeer.split("\n").reduce((a, b) => Number(a) + Number(b), 0)
 );
-const topCalloryCount = Math.max(...totalCalloriesPerReindeer);
-console.log(topCalloryCount);
-const topThreeCalloryCount = totalCalloriesPerReindeer
+const topCalorieCount = Math.max(...totalCaloriesPerReindeer);
+console.log(topCalorieCount);
+const topThreeCalorieCount = totalCaloriesPerReindeer
   .sort((a, b) => b - a)
   .slice(0, 3)
   .reduce((a, b) => a + b, 0);
-console.log(topThreeCalloryCount);
+console.log(topThreeCalorieCount);
