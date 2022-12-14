47f8c254dbe413e6d9a472d288877eaadec2faca
Sophie837 <sophieconstable@yahoo.co.uk>
2022-12-14 10:56:22 +0000

    refactor

--- day-8/index.js
+++ day-8/index.js
@@ -1,110 +1,66 @@
 const { readFileSync, promises: fsPromises } = require("fs");
 const forestMap = readFileSync("day-8/input.txt", "utf-8").split("\n");
-const horizontalTrees = {};
+const verticalTrees = {};
+const scenicScores = {};
 
 for (let i = 0; i < forestMap[0].length; i++) {
   const yTrees = forestMap.map((row) => row[i]);
-  horizontalTrees[i] = yTrees;
-}
-
-function getIndexOfTallerTreesLeftOrAbove(axis, sliceIndex, height) {
-  const treeSlice = axis.slice(0, sliceIndex).reverse();
-  const blockingTreeIndex = treeSlice.findIndex((tree) => tree >= height);
-  return blockingTreeIndex;
-}
-
-function getIndexOfTallerTreesRightOrBelow(axis, sliceIndex, height) {
-  const treeSlice = axis.slice(sliceIndex + 1);
-  const blockingTreeIndex = treeSlice.findIndex((tree) => tree >= height);
-  return blockingTreeIndex;
+  verticalTrees[i] = yTrees;
 }
 
 function getNumberOfVisibleTreesLeftOrAbove(axis, sliceIndex, height) {
-  const blockingTreeIndex = getIndexOfTallerTreesLeftOrAbove(
-    axis,
-    sliceIndex,
-    height
-  );
+  const treeSlice = axis.slice(0, sliceIndex).reverse();
+  const blockingTreeIndex = treeSlice.findIndex((tree) => tree >= height);
   const numberOfVisibleTrees =
-    blockingTreeIndex === -1 ? sliceIndex : blockingTreeIndex + 1;
-  return numberOfVisibleTrees;
-}
-function getNumberOfVisibleTreesRightOrBelow(axis, sliceIndex, height) {
-  const blockingTreeIndex = getIndexOfTallerTreesRightOrBelow(
-    axis,
-    sliceIndex,
-    height
-  );
-  const numberOfVisibleTrees =
-    blockingTreeIndex === -1
-      ? axis.length - sliceIndex - 1
-      : blockingTreeIndex + 1;
-  return numberOfVisibleTrees;
+    blockingTreeIndex === -1 ? treeSlice.length : blockingTreeIndex + 1;
+  return [blockingTreeIndex, numberOfVisibleTrees];
 }
 
-function getNumberOfVisibleTrees() {
+function getNumberOfVisibleTreesRightOrBelow(axis, sliceIndex, height) {
+  const treeSlice = axis.slice(sliceIndex + 1);
+  const blockingTreeIndex = treeSlice.findIndex((tree) => tree >= height);
+  const numberOfVisibleTrees =
+    blockingTreeIndex === -1 ? treeSlice.length : blockingTreeIndex + 1;
+  return [blockingTreeIndex, numberOfVisibleTrees];
+}
+
+function getVisibleAndScenicScores() {
   let hiddenTrees = 0;
   let visibleTrees = 0;
   forestMap.forEach((row, yIndex) => {
     const xTrees = row.split("");
     for (let xIndex = 0; xIndex < xTrees.length; xIndex++) {
-      const yTrees = horizontalTrees[xIndex];
-      // Left
-      getIndexOfTallerTreesLeftOrAbove(xTrees, xIndex, row[xIndex]) === -1
-        ? (visibleTrees += 1)
-        : // Right
-        getIndexOfTallerTreesRightOrBelow(xTrees, xIndex, row[xIndex]) === -1
-        ? (visibleTrees += 1)
-        : // Above
-        getIndexOfTallerTreesLeftOrAbove(yTrees, yIndex, row[xIndex]) === -1
-        ? (visibleTrees += 1)
-        : // Below
-        getIndexOfTallerTreesRightOrBelow(yTrees, yIndex, row[xIndex]) === -1
+      const yTrees = verticalTrees[xIndex];
+      const [blockingTreeIndexLeft, viewingDistanceLeft] =
+        getNumberOfVisibleTreesLeftOrAbove(xTrees, xIndex, row[xIndex]);
+      const [blockingTreeIndexRight, viewingDistanceRight] =
+        getNumberOfVisibleTreesRightOrBelow(xTrees, xIndex, row[xIndex]);
+      const [blockingTreeIndexAbove, viewingDistanceAbove] =
+        getNumberOfVisibleTreesLeftOrAbove(yTrees, yIndex, row[xIndex]);
+      const [blockingTreeIndexBelow, viewingDistanceBelow] =
+        getNumberOfVisibleTreesRightOrBelow(yTrees, yIndex, row[xIndex]);
+
+      blockingTreeIndexLeft === -1 ||
+      blockingTreeIndexRight === -1 ||
+      blockingTreeIndexAbove === -1 ||
+      blockingTreeIndexBelow === -1
         ? (visibleTrees += 1)
         : (hiddenTrees += 1);
-    }
-  });
-  console.log("Visible: ", visibleTrees);
-}
 
-const scenicScores = {};
-function getScenicScores() {
-  forestMap.forEach((row, yIndex) => {
-    const xTrees = row.split("");
-    for (let xIndex = 0; xIndex < xTrees.length; xIndex++) {
-      const viewingDistanceLeft = getNumberOfVisibleTreesLeftOrAbove(
-        xTrees,
-        xIndex,
-        row[xIndex]
-      );
-      const viewingDistanceRight = getNumberOfVisibleTreesRightOrBelow(
-        xTrees,
-        xIndex,
-        row[xIndex]
-      );
-      const yTrees = horizontalTrees[xIndex];
-      const viewingDistanceAbove = getNumberOfVisibleTreesLeftOrAbove(
-        yTrees,
-        yIndex,
-        row[xIndex]
-      );
-      const viewingDistanceBelow = getNumberOfVisibleTreesRightOrBelow(
-        yTrees,
-        yIndex,
-        row[xIndex]
-      );
       const scenicScore =
         viewingDistanceLeft *
         viewingDistanceRight *
         viewingDistanceAbove *
         viewingDistanceBelow;
+
       scenicScores[`${yIndex}-${xIndex}`] = scenicScore;
     }
   });
+  console.log("Visible trees: ", visibleTrees);
 }
 
 function getHighestScore(object) {
-  getScenicScores();
+  getVisibleAndScenicScores();
   let highestScore = 0;
   for (const key in object) {
     if (object[key] > highestScore) {
@@ -114,5 +70,5 @@ function getHighestScore(object) {
   console.log("Highest score: ", highestScore);
 }
 
-getNumberOfVisibleTrees();
+getVisibleAndScenicScores();
 getHighestScore(scenicScores);
